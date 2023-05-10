011819627e34c2ee4315f7337fdeedf60e15a3a7
ListopadovArt <artbdarm9@mail.ru>
2023-05-10 22:38:37 +0300

    init: start project

---
+++ Exgram/ExgramApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExgramApp.swift
+//  Exgram
+//
+//  Created by Artem Listopadov on 10.05.23.
+//
+
+import SwiftUI
+
+@main
+struct ExgramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
