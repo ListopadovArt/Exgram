cf60b4ef961b1b0e80078928a80ff28022947af6
ListopadovArt <artbdarm9@mail.ru>
2023-05-13 20:55:30 +0300

    feat: create CreateNewPost

---
+++ Exgram/View/MainView/CreateNewPost.swift
@@ -0,0 +1,211 @@
+//
+//  CreateNewPost.swift
+//  Exgram
+//
+//  Created by Artem Listopadov on 13.05.23.
+//
+
+import SwiftUI
+import PhotosUI
+import Firebase
+import FirebaseStorage
+
+struct CreateNewPost: View {
+    /// - Callbacks
+    var onPost: (Post)->()
+    /// - Post Properties
+    @State private var postText: String = ""
+    @State private var postImageData: Data?
+    /// - Stored User Data From UserDefaults (AppStorage)
+    @AppStorage("user_profile_url") private var profileURL: URL?
+    @AppStorage("user_name") private var userName: String = ""
+    @AppStorage("user_UID") private var userUID: String = ""
+    /// - View Properties
+    @Environment(\.dismiss) var dismiss
+    @State private var isLoading: Bool = false
+    @State private var errorMessage: String = ""
+    @State private var showError: Bool = false
+    @State private var showImagePicker: Bool = false
+    @State private var photoItem: PhotosPickerItem?
+    @FocusState private var showKeyboard: Bool // Used to toggle the keyboard ON and OFF
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Menu {
+                    Button("Cancel", role: .destructive) {
+                        dismiss()
+                    }
+                } label: {
+                    Text("Cancel")
+                        .font(.callout)
+                        .foregroundColor(.black)
+                }
+                .hAlign(.leading)
+                
+                Button(action: createPost) {
+                    Text("Post")
+                        .font(.callout)
+                        .foregroundColor(.white)
+                        .padding(.horizontal,20)
+                        .padding(.vertical,6)
+                        .background(.black, in: Capsule())
+                }
+                .disableWithOpacity(postText == "")
+            }
+            .padding(.horizontal,15)
+            .padding(.vertical,10)
+            .background {
+                Rectangle()
+                    .fill(.gray.opacity(0.05))
+                    .ignoresSafeArea()
+            }
+            
+            ScrollView(.vertical, showsIndicators: false) {
+                VStack(spacing: 15){
+                    TextField("What's happening?", text: $postText, axis: .vertical)
+                        .focused($showKeyboard)
+                    
+                    if let postImageData, let image = UIImage(data: postImageData) {
+                        GeometryReader {
+                            let size = $0.size
+                            Image(uiImage: image)
+                                .resizable()
+                                .aspectRatio(contentMode: .fill)
+                                .frame(width: size.width, height: size.height)
+                                .clipShape(RoundedRectangle(cornerRadius: 10, style: .continuous))
+                            /// - Delete Button
+                                .overlay(alignment: .topTrailing) {
+                                    Button {
+                                        withAnimation(.easeInOut(duration: 0.25)){
+                                            self.postImageData = nil
+                                        }
+                                    } label: {
+                                        Image(systemName: "trash")
+                                            .fontWeight(.bold)
+                                            .tint(.red)
+                                    }
+                                    .padding(10)
+                                }
+                        }
+                        .clipped()
+                        .frame(height: 220)
+                    }
+                }
+                .padding(15)
+            }
+            
+            Divider()
+            
+            HStack {
+                Button{
+                    showImagePicker.toggle()
+                } label: {
+                    Image(systemName: "photo.on.rectangle")
+                        .font(.title3)
+                }
+                .hAlign(.leading)
+                
+                Button("Done"){
+                    showKeyboard = false
+                }
+            }
+            .foregroundColor(.black)
+            .padding(.horizontal,15)
+            .padding(.vertical,10)
+        }
+        .vAlign(.top)
+        .padding(15)
+        .photosPicker(isPresented: $showImagePicker, selection: $photoItem)
+        .onChange(of: photoItem) { newValue in
+            if let newValue {
+                Task {
+                    do {
+                        guard let rawImageData = try await newValue.loadTransferable(type: Data.self), let image = UIImage(data: rawImageData), let compressedImageData = image.jpegData(compressionQuality: 0.5) else { return }
+                        /// Для экономии места хранения, сжимаем изображение.
+                        /// (Не обятальльное действие)
+                        // MARK: UI Must Be Update on Main Thread
+                        await MainActor.run(body: {
+                            // Setting UserDefaults data and Changing App's Auth Status
+                            postImageData = compressedImageData
+                            photoItem = nil
+                        })
+                        
+                    } catch {
+                        
+                    }
+                }
+            }
+        }
+        // MARK: Displaying Alert
+        .alert(errorMessage, isPresented: $showError, actions: {})
+        /// Loading View
+        .overlay {
+            LoadingView(show: $isLoading)
+        }
+    }
+    
+    // MARK: Post Content To Firebase
+    func createPost(){
+        isLoading = true
+        showKeyboard = false
+        Task {
+            do {
+                guard let profileURL = profileURL else {
+                    return
+                }
+                /// Step 1. Uploading Image if any
+                // Используется для удаления записи
+                let imageReferenceID = "\(userUID)\(Date())"
+                let storageRef = Storage.storage().reference().child("Post_Images").child(imageReferenceID)
+                if let postImageData {
+                    let _ = try await storageRef.putDataAsync(postImageData)
+                    let downloadURL = try await storageRef.downloadURL()
+                    
+                    /// Step 3. Create Post Object With Image Id And URL
+                    let post = Post(text: postText, imageURL: downloadURL, imageReferenceID: imageReferenceID, userName: userName, userUID: userUID, userProfileURL: profileURL)
+                    try await createDocumentAtFirebase(post)
+                } else {
+                    /// Step 2. Directly Post Text Data to Firebase (Since there is no Images Present)
+                    let post = Post(text: postText, userName: userName, userUID: userUID, userProfileURL: profileURL)
+                    try await createDocumentAtFirebase(post)
+                }
+                
+            }catch{
+                await setError(error)
+            }
+        }
+    }
+    func createDocumentAtFirebase(_ post: Post) async throws {
+        /// Write Document to Firebase Firestore
+        let _ = try Firestore.firestore().collection("Posts").addDocument(from: post, completion: { error in
+            
+            guard error != nil else {
+                return
+            }
+            /// Post Successfully Stored at Firebase
+            isLoading = false
+            onPost(post)
+            dismiss()
+            
+        })
+    }
+    
+    // MARK: Displaying Errors VIA Alert
+    func setError(_ error: Error) async {
+        // MARK: UI Must Be Update on Main Thread
+        await MainActor.run(body: {
+            errorMessage = error.localizedDescription
+            showError.toggle()
+            isLoading = false
+        })
+    }
+}
+
+struct CreateNewPost_Previews: PreviewProvider {
+    static var previews: some View {
+        CreateNewPost{ _ in
+            
+        }
+    }
+}
