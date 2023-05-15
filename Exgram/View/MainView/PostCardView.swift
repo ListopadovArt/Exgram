e904c864fe293968bbe6e01b8c075e29b8743ea9
ListopadovArt <artbdarm9@mail.ru>
2023-05-15 21:30:21 +0300

    feat: create like/disLike interaction

--- Exgram/View/MainView/PostCardView.swift
+++ Exgram/View/MainView/PostCardView.swift
@@ -7,12 +7,17 @@
 
 import SwiftUI
 import SDWebImageSwiftUI
+import Firebase
+import FirebaseStorage
 
 struct PostCardView: View {
     var post: Post
     // Callbacks
     var onUpdate: (Post)->()
     var onDelete: ()->()
+    // MARK: View Properties
+    @AppStorage("user_UID") private var userUID: String = ""
+    @State private var docListner: ListenerRegistration? // For Live Updates
     
     var body: some View {
         HStack(alignment: .top, spacing: 12) {
@@ -46,8 +51,106 @@ struct PostCardView: View {
                     }
                     .frame(height: 200)
                 }
+                postInteraction()
             }
         }
         .hAlign(.leading)
+        .onAppear{
+            // Когда Post виден на экране, добавляется список документов, в противном случае список удаляется.
+            // Adding Only Once
+            if docListner == nil {
+                guard let postID = post.id else {
+                    return
+                }
+                docListner = Firestore.firestore().collection("Posts").document(postID).addSnapshotListener({ snapshot, error in
+                    if let snapshot {
+                        if snapshot.exists {
+                            // Document Update
+                            // Fetching Update Document
+                            if let updatePost = try? snapshot.data(as: Post.self){
+                                onUpdate(updatePost)
+                            }
+                        } else {
+                            // Document Deleted
+                            onDelete()
+                        }
+                    }
+                })
+            }
+        }
     }
+    
+    // MARK: Like/DisLike Interaction
+    @ViewBuilder
+    func postInteraction() -> some View {
+        HStack(spacing: 6) {
+            Button(action: likePost){
+                // Когда пользователь ставит лайк, мы добавляем UID этого пользователя в массив likedIDs, и в зависимости от того, содержим массив или не содержит UID такого пользователя, то мы меняем изображение на соответствующее
+                Image(systemName: post.likedIDs.contains(userUID) ? "hand.thumbsup.fill" : "hand.thumbsup")
+            }
+            
+            Text("\(post.likedIDs.count)")
+                .font(.caption)
+                .foregroundColor(.gray)
+            
+            Button(action: dislikePost){
+                // Когда пользователь ставит лайк, мы добавляем UID этого пользователя в массив dislikedIDs, и в зависимости от того, содержим массив или не содержит UID такого пользователя, то мы меняем изображение на соответствующее
+                Image(systemName: post.dislikedIDs.contains(userUID) ? "hand.thumbsdown.fill" : "hand.thumbsdown")
+            }
+            .padding(.leading,25)
+            
+            Text("\(post.dislikedIDs.count)")
+                .font(.caption)
+                .foregroundColor(.gray)
+            
+        }
+        .foregroundColor(.black)
+        .padding(.vertical,8)
+    }
+    
+    
+    // MARK: Liking Post
+    func likePost(){
+        Task {
+            guard let postID = post.id else {
+                return
+            }
+            
+            if post.likedIDs.contains(userUID){
+                // Removing User ID From the Arrey
+                try await Firestore.firestore().collection("Posts").document(postID).updateData([
+                    "likedIDs": FieldValue.arrayRemove([userUID])
+                ])
+            } else {
+                // Adding User ID To Liked Array and removing our ID from Disliked Array (if Added in prior)
+                try await Firestore.firestore().collection("Posts").document(postID).updateData([
+                    "likedIDs": FieldValue.arrayUnion([userUID]),
+                    "dislikedIDs": FieldValue.arrayRemove([userUID])
+                ])
+            }
+        }
+    }
+    
+    // MARK: Disliking Post
+    func dislikePost(){
+        Task {
+            guard let postID = post.id else {
+                return
+            }
+            
+            if post.dislikedIDs.contains(userUID){
+                // Removing User ID From the Arrey
+                try await Firestore.firestore().collection("Posts").document(postID).updateData([
+                    "dislikedIDs": FieldValue.arrayRemove([userUID])
+                ])
+            } else {
+                // Adding User ID To Liked Array and removing our ID from Disliked Array (if Added in prior)
+                try await Firestore.firestore().collection("Posts").document(postID).updateData([
+                    "dislikedIDs": FieldValue.arrayUnion([userUID]),
+                    "likedIDs": FieldValue.arrayRemove([userUID])
+                ])
+            }
+        }
+    }
+    
 }
--- Exgram/View/MainView/ReusablePostView.swift
+++ Exgram/View/MainView/ReusablePostView.swift
@@ -62,9 +62,19 @@ struct ReusablePostView: View {
     func Posts() -> some View {
         ForEach(posts){post in
             PostCardView(post: post) { updatedPost in
-                
+                // Updating Post in the Array
+                if let index = posts.firstIndex(where: { post in
+                    post.id == updatedPost.id
+                }){
+                    posts[index].likedIDs = updatedPost.likedIDs
+                    posts[index].dislikedIDs = updatedPost.dislikedIDs
+                    
+                }
             } onDelete: {
-                
+                // Removing Post From the Array
+                withAnimation(.easeInOut(duration: 0.25)){
+                    posts.removeAll{post == $0}
+                }
             }
             Divider()
                 .padding(.horizontal,-15)
